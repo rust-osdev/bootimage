c02da599d7dd276864e2d3b07ced8ec6ee634bbe
acheronfail <acheronfail@gmail.com>
2018-11-23 12:21:30 +1100

    made bootimage tests run in parallel with rayon

--- src/test.rs
+++ src/test.rs
@@ -1,6 +1,7 @@
 use args::Args;
 use build;
 use failure::{Error, ResultExt};
+use rayon::prelude::*;
 use std::io::Write;
 use std::path::Path;
 use std::time::Duration;
@@ -29,88 +30,88 @@ pub(crate) fn test(args: Args) -> Result<(), Error> {
         test_config
     };
 
-    let mut tests = Vec::new();
-
-    let crate_metadata = metadata
+    let tests = metadata
         .packages
         .iter()
         .find(|p| Path::new(&p.manifest_path) == config.manifest_path)
-        .expect("Could not read crate name from cargo metadata");
-    let target_iter = crate_metadata.targets.iter();
-    for target in target_iter.filter(|t| t.kind == ["bin"] && t.name.starts_with("test-")) {
-        println!("{}", target.name);
+        .expect("Could not read crate name from cargo metadata")
+        .targets
+        .par_iter()
+        .filter(|t| t.kind == ["bin"] && t.name.starts_with("test-"))
+        .map(|target| {
+            println!("RUN: {}", target.name);
 
-        let mut target_args = test_args.clone();
-        target_args.set_bin_name(target.name.clone());
-        let test_path = build::build_impl(
-            &target_args,
-            &test_config,
-            &metadata,
-            &root_dir,
-            &out_dir,
-            false,
-        )?;
+            let mut target_args = test_args.clone();
+            target_args.set_bin_name(target.name.clone());
+            let test_path = build::build_impl(
+                &target_args,
+                &test_config,
+                &metadata,
+                &root_dir,
+                &out_dir,
+                false,
+            )?;
 
-        let test_result;
-        let output_file = format!("{}-output.txt", test_path.display());
+            let test_result;
+            let output_file = format!("{}-output.txt", test_path.display());
 
-        let mut command = process::Command::new("qemu-system-x86_64");
-        command.arg("-drive");
-        command.arg(format!("format=raw,file={}", test_path.display()));
-        command.arg("-device");
-        command.arg("isa-debug-exit,iobase=0xf4,iosize=0x04");
-        command.arg("-display");
-        command.arg("none");
-        command.arg("-serial");
-        command.arg(format!("file:{}", output_file));
-        command.stderr(process::Stdio::null());
-        let mut child = command
-            .spawn()
-            .with_context(|e| format_err!("Failed to launch QEMU: {:?}\n{}", command, e))?;
-        let timeout = Duration::from_secs(60);
-        match child
-            .wait_timeout(timeout)
-            .with_context(|e| format!("Failed to wait with timeout: {}", e))?
-        {
-            None => {
-                child
-                    .kill()
-                    .with_context(|e| format!("Failed to kill QEMU: {}", e))?;
-                child
-                    .wait()
-                    .with_context(|e| format!("Failed to wait for QEMU process: {}", e))?;
-                test_result = TestResult::TimedOut;
-                writeln!(io::stderr(), "Timed Out")?;
-            }
-            Some(_) => {
-                let output = fs::read_to_string(&output_file).with_context(|e| {
-                    format_err!("Failed to read test output file {}: {}", output_file, e)
-                })?;
-                if output.starts_with("ok\n") {
-                    test_result = TestResult::Ok;
-                    println!("Ok");
-                } else if output.starts_with("failed\n") {
-                    test_result = TestResult::Failed;
-                    writeln!(io::stderr(), "Failed:")?;
-                    for line in output[7..].lines() {
-                        writeln!(io::stderr(), "    {}", line)?;
-                    }
-                } else {
-                    test_result = TestResult::Invalid;
-                    writeln!(io::stderr(), "Failed: Invalid Output:")?;
-                    for line in output.lines() {
-                        writeln!(io::stderr(), "    {}", line)?;
+            let mut command = process::Command::new("qemu-system-x86_64");
+            command.arg("-drive");
+            command.arg(format!("format=raw,file={}", test_path.display()));
+            command.arg("-device");
+            command.arg("isa-debug-exit,iobase=0xf4,iosize=0x04");
+            command.arg("-display");
+            command.arg("none");
+            command.arg("-serial");
+            command.arg(format!("file:{}", output_file));
+            command.stderr(process::Stdio::null());
+            let mut child = command
+                .spawn()
+                .with_context(|e| format_err!("Failed to launch QEMU: {:?}\n{}", command, e))?;
+            let timeout = Duration::from_secs(60);
+            match child
+                .wait_timeout(timeout)
+                .with_context(|e| format!("Failed to wait with timeout: {}", e))?
+            {
+                None => {
+                    child
+                        .kill()
+                        .with_context(|e| format!("Failed to kill QEMU: {}", e))?;
+                    child
+                        .wait()
+                        .with_context(|e| format!("Failed to wait for QEMU process: {}", e))?;
+                    test_result = TestResult::TimedOut;
+                    writeln!(io::stderr(), "Timed Out")?;
+                }
+                Some(_) => {
+                    let output = fs::read_to_string(&output_file).with_context(|e| {
+                        format_err!("Failed to read test output file {}: {}", output_file, e)
+                    })?;
+                    if output.starts_with("ok\n") {
+                        test_result = TestResult::Ok;
+                        println!("OK: {}", target.name);
+                    } else if output.starts_with("failed\n") {
+                        test_result = TestResult::Failed;
+                        writeln!(io::stderr(), "Failed:")?;
+                        for line in output[7..].lines() {
+                            writeln!(io::stderr(), "    {}", line)?;
+                        }
+                    } else {
+                        test_result = TestResult::Invalid;
+                        writeln!(io::stderr(), "Failed: Invalid Output:")?;
+                        for line in output.lines() {
+                            writeln!(io::stderr(), "    {}", line)?;
+                        }
                     }
                 }
             }
-        }
-        println!("");
 
-        tests.push((target.name.clone(), test_result))
-    }
+            Ok((target.name.clone(), test_result))
+        })
+        .collect::<Result<Vec<(String, TestResult)>, Error>>()?;
 
     if tests.iter().all(|t| t.1 == TestResult::Ok) {
-        println!("All tests succeeded.");
+        println!("\nAll tests succeeded.");
         Ok(())
     } else {
         writeln!(io::stderr(), "The following tests failed:")?;
