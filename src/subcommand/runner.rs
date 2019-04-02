80aeb7cf835ecc3f9deb60f9f74406101c1a3e80
Philipp Oppermann <dev@phil-opp.com>
2019-04-02 16:54:32 +0200

    Place bootimage-*.bin in default target directory for `runner`
    This is the same behavior as `bootimage run`.

--- src/subcommand/runner.rs
+++ src/subcommand/runner.rs
@@ -1,14 +1,11 @@
 use crate::{args::RunnerArgs, builder::Builder, config, ErrorString};
-use std::{fs, process};
+use std::process;
 
 pub(crate) fn runner(args: RunnerArgs) -> Result<i32, ErrorString> {
     let builder = Builder::new(None)?;
     let config = config::read_config(builder.kernel_manifest_path().to_owned())?;
 
     let bootimage_bin = {
-        let kernel_target_dir = &builder.kernel_metadata().target_directory;
-        let bootimage_target_dir = kernel_target_dir.join("bootimage").join("runner");
-
         let parent = args
             .executable
             .parent()
@@ -19,22 +16,7 @@ pub(crate) fn runner(args: RunnerArgs) -> Result<i32, ErrorString> {
             .ok_or("kernel executable has no file stem")?
             .to_str()
             .ok_or("kernel executable file stem is not valid UTF-8")?;
-        let sub_path = parent.strip_prefix(kernel_target_dir).map_err(|err| {
-            format!(
-                "kernel executable does not live in kernel target directory: {}",
-                err
-            )
-        })?;
-
-        let out_dir = bootimage_target_dir.join(sub_path);
-        fs::create_dir_all(&out_dir).map_err(|err| {
-            format!(
-                "failed to create output directory {}: {}",
-                out_dir.display(),
-                err
-            )
-        })?;
-        out_dir.join(format!("bootimage-{}.bin", file_stem))
+        parent.join(format!("bootimage-{}.bin", file_stem))
     };
 
     builder.create_bootimage(&args.executable, &bootimage_bin, false)?;
